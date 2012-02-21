3f0300f3db4811ede1f05ca0019497ca031de852
ChaeMin Seo <chmin.seo@gmail.com>
2012-02-22 05:29:21 +0900

    HwpHexViewer에 생성자 추가

--- src/soopia/hwp/hexview/HwpHexViewer.java
+++ src/soopia/hwp/hexview/HwpHexViewer.java
@@ -1,6 +1,10 @@
 package soopia.hwp.hexview;
 
 public class HwpHexViewer {
+	
+	public HwpHexViewer(){
+		
+	}
 	public static void main(String[] args) {
 		System.out.println("init hexviewer");
 	}
