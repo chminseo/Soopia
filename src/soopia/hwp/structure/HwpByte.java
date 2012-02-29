a1c2666f954e3a91ee04bf218771b942a88acada
ChaeMin Seo <chmin.seo@gmail.com>
2012-02-29 23:13:54 +0900

    github에서 한글 깨짐

--- src/soopia/hwp/structure/HwpByte.java
+++ src/soopia/hwp/structure/HwpByte.java
@@ -4,14 +4,14 @@ import java.nio.ByteBuffer;
 
 import soopia.hwp.util.Converter;
 /**
- * 부호없는 1 바이트 (0~255, p6)
+ * 부호없는 1 바이트 (0~255, p6 참고 )
  * @author yeori
  *
  */
 public class HwpByte extends AbstractType<Integer> {
 	/**
-	 * 주어진 버퍼 src 에서 offset 위치에서 HwpByte 인스턴스를 생성한다.
-	 * (부호없는 1 바이트, 0~255)
+	 * 주어진 버퍼 src 에서 offset 위치에서 HwpByte 인스턴스를 생성한다.( 0~255 )
+	 * (부호없는 1 바이트, 
 	 * @param offset
 	 * @param src
 	 */
