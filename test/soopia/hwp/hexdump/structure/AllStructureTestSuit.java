71ff28499d731916b0492bf05e1188eb26cc521e
ChaeMin Seo <chmin.seo@gmail.com>
2012-03-03 15:28:48 +0900

    test suit 추가

---
+++ test/soopia/hwp/hexdump/structure/AllStructureTestSuit.java
@@ -0,0 +1,20 @@
+package soopia.hwp.hexdump.structure;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ 
+	TestDword.class,
+	TestHwpByte.class,
+	TestRecordStructure.class,
+	TestStreamStructureFactory.class,
+	TestUInt16.class,
+	TestUInt32.class,
+	TestWChar.class,
+	TestWord.class
+})
+public class AllStructureTestSuit {
+
+}
