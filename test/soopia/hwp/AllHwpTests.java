701fed4ca8c2b7e1b03e5298dd1292a161c51f39
ChaeMin Seo <chmin.seo@gmail.com>
2012-03-22 11:39:14 +0900

    최상위 TestSuit 정의

---
+++ test/soopia/hwp/AllHwpTests.java
@@ -0,0 +1,14 @@
+package soopia.hwp;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({
+	soopia.hwp.codec.AllCodecTests.class
+	,soopia.hwp.type.AllStructureTestSuit.class
+})
+public class AllHwpTests {
+
+}
